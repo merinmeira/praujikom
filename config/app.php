8fdc0715e62e0c22b2f8b66a1bfb40530443b6d7
merin-ujikom2 <merinmoy4@gmail.com>
2019-08-13 11:22:51 +0700

    konfig

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // Add on
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
